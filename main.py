84e1141ae68e36d350f30cf30faf4c5069939ccf
HeRmIoN1 <112274438+HeRmIoN1@users.noreply.github.com>
2023-02-18 20:29:24 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from discord.embeds import Embed
 
 
-client = commands.Bot(command_prefix="$")
+client = commands.Bot(command_prefix="$" , intents = discord.Intents.all())
 client.remove_command("help")
 
 token = "you'r bot token"
@@ -13,16 +13,18 @@ async def on_ready():
     print(f"We have logged in as...\n{client.user}")
 
 @client.command()
-async def massunban(ctx):
-    embed=discord.Embed()
-    embed.add_field(name="unban started!", value="https://github.com/HeRmIoN1")
-    await ctx.send(embed=embed)
-    blist = await ctx.guild.bans()
-    for users in blist:
+async def unban(ctx):
+    counter = 0
+    banned = ctx.guild.bans()
+    async for users in banned:
+        user = users.user
         try:
-            await ctx.guild.unban(user=users.user)
-        except Exception as e:
-            print(f'[ERROR] : {e}')
+            await ctx.guild.unban(user)
+            print(Fore.GRENN + f"Unbanned user: {user}")
+        except:
+            print(Fore.RED + f"Can't Unbanned user: {user}")
+        counter += 1
+    await ctx.channel.send(f"Unbanned {counter} members")
 
 @client.event
 async def on_member_unban(guild, user):
